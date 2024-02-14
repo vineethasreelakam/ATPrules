001d340d4c4aac7c69feebfbcc3ea5431d02871c
vineethasreelakam <vineetha@qualcast.in>
2024-02-14 11:24:05 +0530

    Intendation and italics added in rule15.js

--- Rules/Rule15.js
+++ Rules/Rule15.js
@@ -21,70 +21,70 @@ export default function Rule15({ navigation }) {
             (Commitment Players).{'\n\n'}
 
             <HighlightText style={RuleStyles.SubHeading2}>B.Restrictions</HighlightText>{'\n\n'}
-            1) Special events are those other than Grand Slams, ATP Tour tournaments or ATP
+            {'/t'}1) Special events are those other than Grand Slams, ATP Tour tournaments or ATP
             Challenger Tour tournaments. A player may not compete in a special event if it is
             scheduled as follows:{'\n\n'}
 
-            a) Within the tournament weeks of any ATP Tour Masters 1000 tournament, ATP
+            {'/t/t'}a) Within the tournament weeks of any ATP Tour Masters 1000 tournament, ATP
             Tour 500 tournament and the Nitto ATP Finals (singles or doubles). A player
             who has qualified for and chooses not to play in the ATP Tour Doubles Championship
             shall not be eligible for an exemption to play in any special event
             during that week.{'\n\n'}
 
-            b) Within thirty (30) days before or after the tournament weeks of any ATP Tour
+            {'/t/t'}b) Within thirty (30) days before or after the tournament weeks of any ATP Tour
             Masters 1000 tournament, ATP Tour 500 tournament, the Nitto ATP Finals
             (singles or doubles), if the special event is located within:{'\n\n'}
 
-            i) One hundred (100) miles or one hundred sixty (160) kilometers of the
+            {'/t/t/t'}i) One hundred (100) miles or one hundred sixty (160) kilometers of the
             same; or{'\n\n'}
 
-            ii) The same market area of the city where the tournament is located as
+            {'/t/t/t'}ii) The same market area of the city where the tournament is located as
             determined by the ATP CEO.{'\n\n'}
 
-            c) Within the period of any ATP Tour 250 tournament (qualifying competition and
+            {'/t/t'}c) Within the period of any ATP Tour 250 tournament (qualifying competition and
             main draw) if the special event is located within:{'\n\n'}
 
-            i) One hundred (100) miles or one hundred sixty (160) kilometers of the
+            {'/t/t/t'}i) One hundred (100) miles or one hundred sixty (160) kilometers of the
             same; or{'\n\n'}
 
-            ii) The same market area of the city where the tournament is located as
+            {'/t/t/t'}ii) The same market area of the city where the tournament is located as
             determined by the ATP CEO.{'\n\n'}
 
-            2) Within the tournament week of any ATP Tour or ATP Challenger Tour tournament
+            {'/t'}2) Within the tournament week of any ATP Tour or ATP Challenger Tour tournament
             in which he is entered including the Sunday night after such tournament final,
             except as otherwise expressly permitted in the following subsection C.{'\n\n'}
 
             C.A player who is entered into an ATP Tour 250 tournament may participate in a special
             event on the Monday of such tournament, provided:{'\n\n'}
 
-            1) He has the permission of the Tournament Director of the ATP Tour 250 tournament
+            {'/t'}1) He has the permission of the Tournament Director of the ATP Tour 250 tournament
             he is entered in that week; and{'\n\n'}
 
-            2) If he competes in a tournament during the preceding week, he agrees to complete
+            {'/t'}2) If he competes in a tournament during the preceding week, he agrees to complete
             the same if rain or other unavoidable circumstances delay the finals until Monday;
             and{'\n\n'}
 
-            3) Such special event is not located within:{'\n\n'}
+            {'/t'}3) Such special event is not located within:{'\n\n'}
 
-            a) One hundred (100) miles or one hundred sixty (160) kilometers of the same;
+            {'/t/t'}a) One hundred (100) miles or one hundred sixty (160) kilometers of the same;
             or{'\n\n'}
 
-            b) The same market area of the city where the tournament is located as determined
+            {'/t/t'}b) The same market area of the city where the tournament is located as determined
             by the ATP CEO; or{'\n\n'}
 
-            c) Such special event does not violate any of the other restrictions of this rule.{'\n\n'}
+            {'/t/t'}c) Such special event does not violate any of the other restrictions of this rule.{'\n\n'}
 
             D.A player has the right to petition the ATP CEO or his designee for an exemption to
             these requirements unless otherwise specified above.{'\n\n'}
 
-            Case: May a player, who is not a Commitment Player after losing,
+            {'/t/t/t'}<HighlightText style={{ fontStyle: 'italic' }}> <HighlightText style={{fontWeight: 'bold' }}>Case:</HighlightText> May a player, who is not a Commitment Player after losing,
             retiring or withdrawing from the qualifying, play in a special event
             held the following week end?{'\n\n'}
 
-            Decision: Yes. However, the player must still ask for and receive
+            {'/t/t/t'}<HighlightText style={{fontWeight: 'bold' }}>Decision:</HighlightText> Yes. However, the player must still ask for and receive
             a release from the Supervisor prior to participating in such special
             event. (Also see provisions listed in section 7.06 One Tournament
-            Per Week and Exception){'\n\n'}
+            Per Week and Exception){'\n\n'}</HighlightText>
 
             E. Violations of this section are subject to penalties described under the player Major
             Offense Conduct Contrary to the Integrity of the Game.
